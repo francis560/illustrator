903c0dee6dcbc4a968bf7320ceeba1ff7ed22836
francis560 <francispinales94@gmail.com>
2022-06-22 20:31:37 -0400

    Modo responsive terminado

--- components/footer.jsx
+++ components/footer.jsx
@@ -2,12 +2,12 @@ const Footer = () => {
 
     return (
 
-        <footer className="p-4 flex flex-row aling-center justify-center">
+        <footer className="p-4 flex flex-row aling-center justify-center space-x-8">
 
-            <i className="fi fi-brands-instagram text-slate-900 text-lg"></i>
-            <i className="fi fi-brands-youtube text-slate-900 text-lg"></i>
-            <i className="fi fi-brands-twitter text-slate-900 text-lg"></i>
-            <i className="fi fi-brands-dribbble text-slate-900 text-lg"></i>
+            <i className="fi fi-brands-instagram text-slate-900 text-xl"></i>
+            <i className="fi fi-brands-youtube text-slate-900 text-xl"></i>
+            <i className="fi fi-brands-twitter text-slate-900 text-xl"></i>
+            <i className="fi fi-brands-dribbble text-slate-900 text-xl"></i>
 
         </footer>
 
